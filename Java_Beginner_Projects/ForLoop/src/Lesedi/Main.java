01cdac81202bb1aff4883e37a351c0a619ff346c
Lesedi <leseditumelo32@gmail.com>
2021-10-12 10:03:38 +0200

    java_beginner_class

---
+++ Java_Beginner_Projects/ForLoop/src/Lesedi/Main.java
@@ -0,0 +1,12 @@
+package Lesedi;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+
+    public static double calculateInterest(double amount, double interestRate) {
+        return (amount * (interestRate/100));
+    }
+}
