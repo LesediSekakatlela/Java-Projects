2c47a5d2d7161c8eff4e67227750bfe291cc7af7
Lesedi <leseditumelo32@gmail.com>
2021-10-22 10:08:03 +0200

    WinDemo

---
+++ Java_Beginner_Projects/Packages/WinDemo/src/com/awtSample/Main.java
@@ -0,0 +1,9 @@
+package com.awtSample;
+
+public class Main {
+
+    public static void main(String[] args) {
+        MyWindow myWindow = new MyWindow("Completed Java");
+        myWindow.setVisible(true);
+    }
+}
