0a00252964bfb29fb6296fdb9365439908165596
LesediSekakatlela <84187934+LesediSekakatlela@users.noreply.github.com>
2022-01-11 08:31:04 +0200

    Update NotHandled.java

--- JavaHerbSchildt/HerbSchildtOracel/src/main/java/Chapter9/NotHandled.java
+++ JavaHerbSchildt/HerbSchildtOracel/src/main/java/Chapter9/NotHandled.java
@@ -10,13 +10,13 @@ package Chapter9;
  * @author admin
  */
 // Let JVM handle the error. 
-//class NotHandled { 
-//  public static void main(String args[]) { 
-//    int nums[] = new int[4]; 
-// 
-//    System.out.println("Before exception is generated."); 
-// 
-//    // generate an index out-of-bounds exception 
-//    nums[7] = 10; 
-//  } 
-//}
+class NotHandled { 
+  public static void main(String args[]) { 
+    int nums[] = new int[4]; 
+ 
+    System.out.println("Before exception is generated."); 
+ 
+    // generate an index out-of-bounds exception 
+    nums[7] = 10; 
+  } 
+}
